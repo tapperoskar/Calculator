925131230bdd3d1c5b3c8b150d652f4e781f6798
Oskar Tapper <tapper.oskar@gmail.com>
2024-11-19 15:48:19 +0100

    added files

---
+++ MyFunctions.java
@@ -0,0 +1,11 @@
+class MyFunctions{
+    public void greet(String name){
+      System.out.println("Hello and welcome "+ name +" to my calculator!");
+    }  
+
+
+    public int add(int a, int b){
+      return a +b;
+    } 
+
+}
